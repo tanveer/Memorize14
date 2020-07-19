8124d5c20da1731f120384d2cc016a5372216939
Tanveer Bashir <tan.m.bashir@gmail.com>
2020-07-19 15:53:57 -0400

    Initial Commit

---
+++ Memorize14/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Memorize14
+//
+//  Created by Tanveer Bashir on 7/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize14/Memorize14App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize14App.swift
+//  Memorize14
+//
+//  Created by Tanveer Bashir on 7/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Memorize14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
